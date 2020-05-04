3f521c82866db23a3c00ed1dd786ae6090f45c4e
Anton <anton.steshko@inspectorio.com>
2020-05-04 20:17:13 +0300

    Remove redundant code

--- src/state/Timer/reducers/restoreTimerStateReducer.js
+++ src/state/Timer/reducers/restoreTimerStateReducer.js
@@ -1,21 +1,5 @@
-import { isPast, addSeconds } from 'date-fns';
-
 import { TIMER_STATUSES } from '../constants';
 
 export default function restoreTimerStateReducer(persistedState) {
-  const nextList = persistedState.list.map(item => {
-    // finish all unfinished
-    const finishedAt = addSeconds(item.startedAt, item.timeTotal);
-    if (!item.finishedAt && isPast(finishedAt)) {
-      return {
-        ...item,
-        timeCompleted: item.timeTotal,
-        finishedAt,
-      };
-    }
-
-    return item;
-  });
-
-  return { ...persistedState, status: TIMER_STATUSES.STARTED, list: nextList };
+  return { ...persistedState, status: TIMER_STATUSES.STARTED };
 }
